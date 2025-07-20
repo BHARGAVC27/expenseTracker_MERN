2ef6f154f5e42c3d11d7d1af14636559bf7c5fad
Bhargav <bhargavc2005@gmail.com>
2025-07-20 11:35:04 +0530

    Added server /get

--- server/server.js
+++ server/server.js
@@ -23,3 +23,7 @@ app.use('/api', incomeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/',(req,res) => {
+    res.send('API is running...');
+})
